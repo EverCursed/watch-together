7c23538a1e954b8b09e8f107ef20b120a22478e6
Valentine Yelizarov <valentine.yelizarov@stonybrook.edu>
2018-09-06 10:07:26 -0400

    Set up building structure

--- src/deb-watchtogether.c
+++ src/deb-watchtogether.c
@@ -1,5 +1,8 @@
 #include <gtk/gtk.h>
 
+#include "version.h"
+
+
 static void
 activate (GtkApplication* app,
           gpointer        user_data)
@@ -7,8 +10,9 @@ activate (GtkApplication* app,
     GtkWidget *window;
     
     window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_window_set_title (GTK_WINDOW (window), WT_WINDOW_TITLE);
+    // TODO(Val): Make this use stored values in .ini
+    gtk_window_set_default_size (GTK_WINDOW (window), 1024, 576);
     gtk_widget_show_all (window);
 }
 
@@ -19,7 +23,8 @@ main (int    argc,
     GtkApplication *app;
     int status;
     
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    // TODO(Val): Figure out how to properly define an id for the app
+    app = gtk_application_new ("com.github.EverCursed.watch-together", G_APPLICATION_FLAGS_NONE);
     g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
     status = g_application_run (G_APPLICATION (app), argc, argv);
     g_object_unref (app);
