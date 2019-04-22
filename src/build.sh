3358f01dfe851601fe1ea6dc7c2f6ec5e63c02a0
Valentine Yelizarov <valentine.yelizarov@stonybrook.edu>
2019-04-22 12:15:30 -0400

    Change building

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 mkdir -p ../bin
 
-gcc -g -o bin/watchtogether src/deb-watchtogether-v2.c -lSDL2 -no-pie -O3
+gcc -g -o bin/watchtogether src/deb-watchtogether-v2.c `pkg-config --cflags --libs libavcodec libavutil libavformat` -lm -lSDL2 -no-pie
