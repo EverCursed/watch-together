ca65994e510e9560183fb97f8f40d526e520bde3
Valentine Yelizarov <valentine.yelizarov@stonybrook.edu>
2019-12-31 17:33:05 -0500

    Add a batch file to pack the application

--- pack.bat
+++ pack.bat
@@ -1,3 +1,18 @@
+REM To use this you will need to create a "lib" directory in the main application directory.
+REM 
+REM The following dll's need to be in there for the full application to pack:
+REM 	avcodec-58.dll
+REM 	avdevice-58.dll
+REM 	avfilter-7.dll
+REM 	avformat-58.dll
+REM 	avutil-56.dll
+REM 	postproc-55.dll
+REM 	SDL2.dll
+REM 	SDL2_net.dll
+REM 	swresample-3.dll
+REM 	swscale-5.dll
+REM 
+
 @echo off
 
 SET folder=watchtogether
